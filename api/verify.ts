c186a315b6a7d75f8186f8db68f0d0beeb19dcc5
Jordan <Jordan19731030@gmail.com>
2021-12-02 10:39:27 +0800

    remove debug

--- api/verify.ts
+++ api/verify.ts
@@ -1,6 +1,6 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
-// import { cryptoWaitReady, decodeAddress, signatureVerify } from '@polkadot/util-crypto';
-// import { u8aToHex } from '@polkadot/util';
+import { cryptoWaitReady, decodeAddress, signatureVerify } from '@polkadot/util-crypto';
+import { u8aToHex } from '@polkadot/util';
 
 const keyPublicKey = "polkadotKey";
 const keyEvmAddr = "evmAddress";
@@ -8,7 +8,7 @@ const keySign = "signature";
 
 export default async (request: VercelRequest, response: VercelResponse) => {
   if(request.headers.authorization !== `Bearer ${process.env.VERCEL_TOKEN}`){
-    return response.status(401).send("invalid token");
+    return response.status(401).send("invalid token.")
   }
   //get params from request
   const polkadotPublicKey = request.query[keyPublicKey];
@@ -17,20 +17,20 @@ export default async (request: VercelRequest, response: VercelResponse) => {
   console.log(`${polkadotPublicKey}: ${evmAddr}: ${sign}`)
 
   let result = false;
-  // if(polkadotPublicKey && typeof polkadotPublicKey === "string" && evmAddr && typeof evmAddr === "string" && sign && typeof sign === "string"){
-  //   await cryptoWaitReady();
-  //   result = isValidSignature(evmAddr, sign, polkadotPublicKey);
-  // }
+  if(polkadotPublicKey && typeof polkadotPublicKey === "string" && evmAddr && typeof evmAddr === "string" && sign && typeof sign === "string"){
+    await cryptoWaitReady();
+    result = isValidSignature(evmAddr, sign, polkadotPublicKey);
+  }
   return response.status(200).send(result);
 };
 
-// const isValidSignature = (signedMessage:string, signature:string, polkadotPubKey:string) => {
-//   try {
-//     const publicKey = decodeAddress(polkadotPubKey);
-//     const hexPublicKey = u8aToHex(publicKey);
-//     return signatureVerify(signedMessage, signature, hexPublicKey).isValid; 
-//   } catch (error) {
-//     console.error(error)
-//     return false;
-//   }
-// };
+const isValidSignature = (signedMessage:string, signature:string, polkadotPubKey:string) => {
+  try {
+    const publicKey = decodeAddress(polkadotPubKey);
+    const hexPublicKey = u8aToHex(publicKey);
+    return signatureVerify(signedMessage, signature, hexPublicKey).isValid; 
+  } catch (error) {
+    console.error(error)
+    return false;
+  }
+};
