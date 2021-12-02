5550435c2309d0d792a411f0fb2af5183538d86d
Jordan <Jordan19731030@gmail.com>
2021-12-02 11:39:21 +0800

    fix -> verify evmAddr

--- api/verify.ts
+++ api/verify.ts
@@ -1,47 +1,65 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
-import { cryptoWaitReady, decodeAddress, signatureVerify } from '@polkadot/util-crypto';
-import { u8aToHex } from '@polkadot/util';
+import { VercelRequest, VercelResponse } from "@vercel/node";
+import {
+  cryptoWaitReady,
+  decodeAddress,
+  signatureVerify,
+} from "@polkadot/util-crypto";
+import { u8aToHex } from "@polkadot/util";
+import { ethers } from "ethers";
 
 const keyPublicKey = "polkadotKey";
 const keyEvmAddr = "evmAddress";
 const keySign = "signature";
 
 export default async (request: VercelRequest, response: VercelResponse) => {
-  if(request.headers.authorization !== `Bearer ${process.env.VERCEL_TOKEN}`){
-    return response.status(401).json({isValidResult: false})
+  if (request.headers.authorization !== `Bearer ${process.env.VERCEL_TOKEN}`) {
+    return response.status(401).json({ isValidResult: false });
   }
   //get params from request
   const polkadotPublicKey = request.query[keyPublicKey];
   const evmAddr = request.query[keyEvmAddr];
   const sign = request.query[keySign];
-  console.log(`${polkadotPublicKey}: ${evmAddr}: ${sign}`)
+  console.log(`${polkadotPublicKey}: ${evmAddr}: ${sign}`);
 
-  let result:VercelResult = {
+  let result: VercelResult = {
     isValidResult: true,
-    isValidSign: false
+    isValidSign: false,
   };
-  if(polkadotPublicKey && typeof polkadotPublicKey === "string" && evmAddr && typeof evmAddr === "string" && sign && typeof sign === "string"){
+  if (
+    polkadotPublicKey &&
+    typeof polkadotPublicKey === "string" &&
+    evmAddr &&
+    typeof evmAddr === "string" &&
+    sign &&
+    typeof sign === "string" &&
+    ethers.utils.isAddress(evmAddr)
+  ) {
     await cryptoWaitReady();
     result.isValidSign = isValidSignature(evmAddr, sign, polkadotPublicKey);
-    if(result.isValidSign){
-      result.publicKey = Buffer.from(decodeAddress(polkadotPublicKey)).toString('hex');
+    if (result.isValidSign) {
+      result.publicKey =
+        "0x" + Buffer.from(decodeAddress(polkadotPublicKey)).toString("hex");
     }
   }
   return response.status(200).json(result);
 };
 
-const isValidSignature = (signedMessage:string, signature:string, polkadotPubKey:string) => {
+const isValidSignature = (
+  signedMessage: string,
+  signature: string,
+  polkadotPubKey: string
+) => {
   try {
     const publicKey = decodeAddress(polkadotPubKey);
     const hexPublicKey = u8aToHex(publicKey);
-    return signatureVerify(signedMessage, signature, hexPublicKey).isValid; 
+    return signatureVerify(signedMessage, signature, hexPublicKey).isValid;
   } catch (error) {
-    console.error(error)
+    console.error(error);
     return false;
   }
 };
 
-interface VercelResult{
+interface VercelResult {
   isValidResult: boolean;
   isValidSign?: boolean;
   publicKey?: string;
