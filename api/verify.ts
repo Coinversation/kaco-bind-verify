3fa1fe22e2e044f0eb8afe00c67afe3d8b835bee
Jordan <Jordan19731030@gmail.com>
2021-12-02 09:56:56 +0800

    feat -> add token

--- api/verify.ts
+++ api/verify.ts
@@ -6,28 +6,31 @@ const testPublicAddr = "WuCxWAfuWgGRY6oEABmjorBWAxVjFCEBcaEAAV4cenijcbr";
 const testEvmAddr = "0xFB83a67784F110dC658B19515308A7a95c2bA33A";
 const testSign = "0x96644fa61dce77339b7b07ff49feec44075242f26d5d83d1820c0a055b47087d33b20082ae094cea6d64cd4bb74da18a17c7f3c3f4226de4313e1bbe97e1c983";
 
-const keyPublicAddr = "polkadotKey";
+const keyPublicKey = "polkadotKey";
 const keyEvmAddr = "evmAddress";
 const keySign = "signature";
 
 export default async (request: VercelRequest, response: VercelResponse) => {
+  if(request.headers.authorization !== `Bearer ${process.env.VERCEL_TOKEN}`){
+    return response.status(401)
+  }
   //get params from request
-  const publicAddr = request.query[keyPublicAddr];
+  const polkadotPublicKey = request.query[keyPublicKey];
   const evmAddr = request.query[keyEvmAddr];
   const sign = request.query[keySign];
   // console.log(`${publicAddr}: ${evmAddr}: ${sign}`)
 
   let result = false;
-  if(publicAddr && typeof publicAddr === "string" && evmAddr && typeof evmAddr === "string" && sign && typeof sign === "string"){
+  if(polkadotPublicKey && typeof polkadotPublicKey === "string" && evmAddr && typeof evmAddr === "string" && sign && typeof sign === "string"){
     await cryptoWaitReady();
-    result = isValidSignature(evmAddr, sign, publicAddr);
+    result = isValidSignature(evmAddr, sign, polkadotPublicKey);
   }
-  response.status(200).send(result);
+  return response.status(200).send(result);
 };
 
-const isValidSignature = (signedMessage:string, signature:string, address:string) => {
+const isValidSignature = (signedMessage:string, signature:string, polkadotPubKey:string) => {
   try {
-    const publicKey = decodeAddress(address);
+    const publicKey = decodeAddress(polkadotPubKey);
     const hexPublicKey = u8aToHex(publicKey);
     return signatureVerify(signedMessage, signature, hexPublicKey).isValid; 
   } catch (error) {
